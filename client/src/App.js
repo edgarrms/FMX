0cb77061975ffae84d40aa38c28e23338b238c58
Edgar <ramosjair94@gmail.com>
2020-03-11 20:30:27 -0700

    created react app and added components

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Nav from "./components/nav";
+import Home from "./components/home"
+import './App.css';
+import SignUp from "./components/signup"
+import SignIn from "./components/signin"
+function App() {
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+
+          <Route path="/signup">
+            <SignUp />
+          </Route>
+
+          <Route path="/signin">
+            <SignIn />
+          </Route>
+
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
