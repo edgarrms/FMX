ec29530b5f92f386acc11a9f908852531f4711ca
Edgar <ramosjair94@gmail.com>
2020-03-16 23:47:24 -0700

    added links to regidster, signin, and user->profile

--- client/src/components/nav/index.js
+++ client/src/components/nav/index.js
@@ -6,11 +6,11 @@ export default function Nav() {
     return (
         <div className="navbar navbar-light bg-danger">
             <div>
-                <h1 ><Link className="home-nav" to="home">FMX</Link></h1>
+                <h1 ><Link className="home-nav" to="home">FM<img src="https://img.icons8.com/ios-filled/50/000000/x.png" /></Link></h1>
                 <p>Fu*k My eX!</p></div>
-            <Link to="/profile">Profile </Link>
-            <Link to="/signup">Sign Up</Link>
-            <Link to="/signin">Sign In</Link>
+            {/* <div ><Link className="profile-link" to="/profile">Profile </Link></div> */}
+            <div ><Link className="sign-up-link" to="/signup">Sign Up</Link></div>
+            <div ><Link className="sign-in-link" to="/signin">Sign In</Link></div>
         </div>
 
     );
