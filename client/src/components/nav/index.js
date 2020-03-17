273c8ea6de7081c3fe1bd938afe45746fed4ab37
Edgar <ramosjair94@gmail.com>
2020-03-16 21:26:32 -0700

    merging branches

--- client/src/components/nav/index.js
+++ client/src/components/nav/index.js
@@ -1,11 +1,12 @@
 import React from "react";
+import './index.css';
 import { BrowserRouter as Router, Switch, Link } from "react-router-dom";
 
 export default function Nav() {
     return (
         <div className="navbar navbar-light bg-danger">
             <div>
-                <h1><Link to="home">FMX</Link></h1>
+                <h1 ><Link className="home-nav" to="home">FMX</Link></h1>
                 <p>Fu*k My eX!</p></div>
             <Link to="/profile">Profile </Link>
             <Link to="/signup">Sign Up</Link>
